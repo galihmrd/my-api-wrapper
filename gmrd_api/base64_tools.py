76db7aade29cebcc4bbce1f37f61e21b8d99336f
galihmrd <galihmardiana15@gmail.com>
2024-05-05 00:03:39 +0700

    Added remove background foto

---
+++ gmrd_api/base64_tools.py
@@ -0,0 +1,8 @@
+import base64
+
+class Base64Tool:
+    def image_to_string(file_path):
+        with open(file_path, "rb") as file:
+            file_data = file.read()
+            base64_encoded_data = base64.b64encode(file_data).decode("utf-8")
+            return base64_encoded_data
---
+++ gmrd_api/remove_backgroud.py
@@ -0,0 +1,21 @@
+import os
+import requests
+from .base64_tools import Base64Tool
+
+
+class RemoveBGParser:
+    def __init__(self, data):
+        self.result = data
+        self.url = os.getenv("REST_API") + data.get("url")
+        self.status = data.get("status")
+
+class RemoveBG(RemoveBGParser):
+    def __init__(self, path):
+        converted = Base64Tool.image_to_string(path)
+        payload = {"image": "data:image/jpeg;base64," +  converted, "type": "removebg"}
+        api = os.getenv("REST_API")
+        try:
+           r = requests.post(api + "/removebg", json=payload)
+        except Exception as e:
+           raise Exception("Error: " + str(e))
+        super().__init__(r.json())
